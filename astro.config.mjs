3602dc3e6138b91ec7c905a2e6720559d8d4ad63
Joel Elizaga <joel@elizaga.org>
2022-08-15 20:16:17 -0700

    Replaced Jekyll with Astro. Empty project.

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [solidJs()],
+  markdown: {
+    syntaxHighlight: 'prism'
+  }
+});
