08ac1bf07d1c757398967b1035325161e0f9c770
Joel Elizaga <joel@elizaga.org>
2022-11-29 15:13:45 -0800

    Installed @astrojs/image.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), mdx()],
+  integrations: [solidJs(), mdx(), image()],
   markdown: {
     syntaxHighlight: 'prism'
   }
