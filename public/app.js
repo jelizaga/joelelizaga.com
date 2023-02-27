a587e2d0282f817c35bd42301eff438a19278bb5
Joel Elizaga <joel@elizaga.org>
2023-02-27 10:47:40 -0800

    CSS.

---
+++ public/app.js
@@ -0,0 +1,12 @@
+import Swup from "swup";
+// import Swup from "../node_modules/swup/dist/swup.min.js";
+// import SwupHeadPlugin from "/node_modules/@swup/head-plugin/dist/SwupHeadPlugin.min.js";
+// import SwupA11yPlugin from "/node_modules/@swup/a11y-plugin/dist/SwupA11yPlugin.min.js";
+// import SwupSlideTheme from "/node_modules/@swup/slide-theme/dist/SwupSlideTheme.min.js";
+const swup = new Swup({
+  plugins: [
+    // new SwupHeadPlugin(),
+    // new SwupA11yPlugin(),
+    // new SwupSlideTheme(),
+  ],
+});
