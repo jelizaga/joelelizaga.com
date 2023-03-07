dc10249ca611d774ca3e1c8c3c60108366a40944
Joel Elizaga <joel@elizaga.org>
2023-03-07 15:06:29 -0800

    Featued index projects.

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
 import Swup from "swup";
-// import Swup from "../node_modules/swup/dist/swup.min.js";
+// import SwupHeadPlugin from "@swup/head-plugin";
+// import SwupA11yPlugin from "@swup/a11y-plugin";
+// import SwupSlideTheme from "@swup/slide-theme";
 // import SwupHeadPlugin from "/node_modules/@swup/head-plugin/dist/SwupHeadPlugin.min.js";
 // import SwupA11yPlugin from "/node_modules/@swup/a11y-plugin/dist/SwupA11yPlugin.min.js";
 // import SwupSlideTheme from "/node_modules/@swup/slide-theme/dist/SwupSlideTheme.min.js";
