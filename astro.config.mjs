f027349dcd3a6b0080a0294057fbcd121e40c2d1
Joel Elizaga <joel@elizaga.org>
2023-08-18 21:40:26 -0700

    Astro v1 → Astro v2 #50

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://joelelizaga.com/",
   integrations: [
     solidJs(),
     mdx(),
