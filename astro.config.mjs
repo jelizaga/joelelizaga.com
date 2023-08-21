b1178e87d8c4c2d5f7409aaf9b56e696d9efa7e1
Joel Elizaga <joel@elizaga.org>
2023-08-21 16:01:53 -0700

    Removed @astro/image: deprecated.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,12 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://joelelizaga.com/",
   integrations: [
     solidJs(),
     mdx(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    })
   ],
   markdown: {
     syntaxHighlight: 'prism',
