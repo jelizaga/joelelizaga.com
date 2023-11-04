2983ce2113fd47a513cde7a4c400677e72c03dc9
Joel Elizaga <joel@elizaga.org>
2023-11-04 16:09:50 -0700

    Fixed caption styling.

--- src/components/NNImagePopupCaption.tsx
+++ src/components/NNImagePopupCaption.tsx
@@ -7,10 +7,9 @@ import { SolidMarkdown } from "solid-markdown"
 export default function NNImagePopupCaption(props: any) {
   //////////////////////////////////////////////////////////////////////////////
   return (
-    <p class="caption">
-      <SolidMarkdown
-        children={props.caption}
-      />
-    </p>
+    <SolidMarkdown
+      class="caption"
+      children={props.caption}
+    />
   )
 }
